43e5195d0ee928754c0b39f5406111e7bbe42475
anto19900 <ababorrelli4@gmail.com>
2018-06-06 18:37:52 +0200

    non pullare qui

--- app/src/main/java/com/example/ababo/progettoarduinouniversit/StanzeAdapter.java
+++ app/src/main/java/com/example/ababo/progettoarduinouniversit/StanzeAdapter.java
@@ -5,13 +5,21 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.ListAdapter;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.DataSnapshot;
 import com.example.ababo.progettoarduinouniversit.datamodel.Stanza;
 
 import java.util.List;
 
 public class StanzeAdapter extends BaseAdapter {
+    FirebaseDatabase firebaseDatabase = FirebaseDatabase.getInstance();
+    DatabaseReference myRef = firebaseDatabase.getReference("Stanze");
     private Context context;
     private List<Stanza> elencoStanze;
 
@@ -55,6 +63,8 @@ public class StanzeAdapter extends BaseAdapter {
         TextView vNomeCompleto = view.findViewById(R.id.textNome);
 
         // Imposto i valori da visualizzare nella list view
+
+
         Stanza s = elencoStanze.get(i);
         vMatricola.setText(s.getMatricola());
         vNomeCompleto.setText(s.getNome());
@@ -63,6 +73,50 @@ public class StanzeAdapter extends BaseAdapter {
         // Restituisco la view alla lista
         return view;
     }
+
+    public  void refreshdata() {
+        firebaseDatabase.addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+                getupdates(dataSnapshot);
+            }
+
+            @Override
+            public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+                getupdates(dataSnapshot);
+            }
+
+            @Override
+            public void onChildRemoved(DataSnapshot dataSnapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onCancelled(FirebaseError firebaseError) {
+
+            }
+        });
+    }
+
+
+    public void getupdates(DataSnapshot dataSnapshot) {
+
+        elencoStanze.clear();
+        for (DataSnapshot ds : dataSnapshot.getChildren()) {
+            Stanza s = new Stanza();
+            s.setNome("ciao");
+            s.setDispositivi(2);
+            s.setMatricola("ciao");
+            elencoStanze.add(s);
+        }
+
+        }
+
 
     public void setElencoStanze(List<Stanza> elencoStanze) {
         this.elencoStanze = elencoStanze;
